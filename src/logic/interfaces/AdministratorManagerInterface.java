f748589dc1b55d73169154a45439916e4231e343
Jesus Tlapa <jesus.tlapa11@gmail.com>
2024-03-20 11:42:21 -0600

    Eliminacion de cosas que ya no funcionan

--- src/logic/interfaces/AdministratorManagerInterface.java
+++
@@ -1,9 +0,0 @@
-package logic.interfaces;
-
-import logic.domain.Administrator;
-
-public interface AdministratorManagerInterface {
-    int insertAdministrator(Administrator administrator);
-    Administrator getAdministratorByIdUser(int id);
-    Administrator getAdministratorByIdAdministrator(int id);
-}
