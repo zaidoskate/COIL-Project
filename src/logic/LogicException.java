ef9f7bc46024fedafe5ff64df8a077bc49a13c04
Zaid <zaidoskate@hotmail.com>
2024-06-10 15:27:34 -0600

    Mensajes perso error BD

--- src/logic/LogicException.java
+++ src/logic/LogicException.java
@@ -4,13 +4,29 @@
  */
 package logic;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author zaido
  */
 public class LogicException extends Exception {
-    
+
     public LogicException(String message, Throwable cause) {
-        super(message, cause);
+        super(buildMessage(message, cause), cause);
+    }
+
+    private static String buildMessage(String message, Throwable cause) {
+        if (cause instanceof SQLException) {
+            SQLException sqlException = (SQLException) cause;
+            switch (sqlException.getErrorCode()) {
+                case 1045:
+                    return message = "Error de autenticación: usuario o contraseña de la base de datos incorrectos ";
+                case 1049:
+                    return message = "Error de conexión: base de datos desconocida";
+                default:
+            }
+        }
+        return message;
     }
 }
